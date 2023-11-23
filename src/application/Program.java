f2c19248a58e178742d642396ddba5f052763bdc
Fernando Aguiar <fernando2015.aguiar@gmail.com>
2023-11-23 15:43:04 -0300

    heranca_java

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BusinessAccount account = new BusinessAccount();
+		
+		account.getBalance();
+
+	}
+
+}
